0478762346697651d00f49e82c2a66cf3ab1a77d
dbc2201 <divyanshb22@gmail.com>
2020-08-21 15:49:36 +0530

    create Book.java

---
+++ src/definitions/Book.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 21/08/20
+ *  Time: 3:49 PM
+ *  File Name : Book.java
+ * */
+package definitions;
+
+public class Book {
+}
