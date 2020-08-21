ecaae42eb5e624a5d57a2be7a4c31b1b039ae8fc
dbc2201 <divyanshb22@gmail.com>
2020-08-21 15:50:41 +0530

    create FrontDesk.java

---
+++ src/execution/FrontDesk.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 21/08/20
+ *  Time: 3:50 PM
+ *  File Name : FrontDesk.java
+ * */
+package execution;
+
+public class FrontDesk {
+}
