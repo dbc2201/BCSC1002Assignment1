009622392605de0495068440983dfa51d11ba8cd
dbc2201 <divyanshb22@gmail.com>
2020-09-05 11:38:54 +0530

    create getters and setters for the private fields of Student class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -11,4 +11,36 @@ public class Student {
     private String middleName;
     private String lastName;
     private String fullName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
