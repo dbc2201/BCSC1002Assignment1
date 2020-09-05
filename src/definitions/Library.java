8abe3254c0180c3ce0734bcc4c5f08fd26b3f799
dbc2201 <divyanshb22@gmail.com>
2020-09-05 11:31:22 +0530

    add a toString() method for the Library class

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -6,6 +6,43 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Library {
     private Book[] books;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Library library = (Library) o;
+        return Arrays.equals(getBooks(), library.getBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getBooks());
+    }
+
+    public Book[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder bookList = new StringBuilder();
+        for (Book book : this.getBooks()) {
+            bookList.append(book.toString());
+            bookList.append("-----");
+        }
+        return bookList.toString();
+    }
 }
