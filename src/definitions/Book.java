8fb995a71ffcd9295ebd3e8420c6e0fdca3d0e63
dbc2201 <divyanshb22@gmail.com>
2020-09-05 11:25:40 +0530

    create a parameterized constructor for the Book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -10,4 +10,11 @@ public class Book {
     private String name;
     private String authorName;
     private String isbn;
+
+    public Book(String name, String authorName, String isbn) {
+        this.name = name;
+        this.authorName = authorName;
+        this.isbn = isbn;
+    }
+
 }
